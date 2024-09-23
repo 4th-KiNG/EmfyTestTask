3e63098bce84136f650e6e76e9c17bc455d81fee
4th-KiNG <95131486+4th-KiNG@users.noreply.github.com>
2024-09-24 01:10:28 +0300

    123

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -4,6 +4,7 @@ const MainPage = () => {
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const authorizationCode = params.get("code");
+    console.log(authorizationCode);
   }, []);
   return (
     <>
